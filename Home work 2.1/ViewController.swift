be70dbcae3149ad23e4632bc030d12370009fb1f
Elena <aalazuka@gmail.com>
2020-08-19 18:47:10 +0300

    Initial Commit

---
+++ Home work 2.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Home work 2.1
+//
+//  Created by Елена Лазука on 19.08.2020.
+//  Copyright © 2020 Елена Лазука. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
